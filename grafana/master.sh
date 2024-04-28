6acf7551e1fcdf2a351cbcea4d55706ad9825cec
Your Name <you@example.com>
2024-04-28 16:33:19 +0300

    apply

--- grafana/master.sh
+++ grafana/master.sh
@@ -3,5 +3,8 @@
 wget -qO- https://raw.githubusercontent.com/voutuk/file/main/swap.sh | sudo bash
 wget -qO- https://raw.githubusercontent.com/voutuk/file/main/docker.sh | sudo bash
 
-git clone https://github.com/voutuk/docker-cluster
+git clone https://github.com/voutuk/PAG
 
+# docker swarm init
+# cd docker
+# docker stack deploy -c docker-compoose.yml global --detach=false
--- terraform/main.tf
+++ terraform/main.tf
@@ -22,146 +22,44 @@ provider "aws" {
 
 
 // MasterHost
-resource "aws_instance" "AWS-0" {
-  availability_zone = "eu-north-1a"
-  ami = "ami-0014ce3e52359afbd"
-  instance_type = "t3.micro"
-  key_name = "Stepik1"
-  vpc_security_group_ids = [aws_security_group.AWS-1.id]
+resource "aws_instance" "master_host" {
+  availability_zone       = "eu-north-1a"
+  ami                     = "ami-0014ce3e52359afbd"
+  instance_type           = "t3.micro"
+  key_name                = "Stepik1"
+  vpc_security_group_ids  = [aws_security_group.master_host.id] // Properly reference security group
   ebs_block_device {
     device_name = "/dev/sda1"
     volume_size = 10
     volume_type = "standard"
-    tags = {device_name = "Terraform"}
+    tags        = { device_name = "Terraform" }
   }
   tags = { 
     Name = "MasterHost"
   }
-  provisioner "file" {                               # OFF SLOW OFF key error
-    source      = "${path.module}/master.sh"
-    destination = "/tmp/script.sh"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/script.sh",
-      "sudo bash /tmp/script.sh",
-    ]
-  }
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    password    = ""
-    private_key = file("${path.module}/Stepik1.pem")
-    host        = self.public_ip
-  }
 }
 
-// Node1
-resource "aws_instance" "AWS-1" {
-  availability_zone = "eu-north-1a"
-  ami = "ami-0014ce3e52359afbd"
-  instance_type = "t3.micro"
-  key_name = "Stepik1"
-  vpc_security_group_ids = [aws_security_group.AWS-1.id]
+resource "aws_instance" "node" {
+  count                   = 3
+  availability_zone       = "eu-north-1a"
+  ami                     = "ami-0014ce3e52359afbd"
+  instance_type           = "t3.micro"
+  key_name                = "Stepik1"
+  vpc_security_group_ids  = [aws_security_group.nodes.id] // Properly reference security group
   ebs_block_device {
     device_name = "/dev/sda1"
     volume_size = 10
     volume_type = "standard"
-    tags = {device_name = "Terraform"}
+    tags        = { device_name = "Terraform" }
   }
   tags = { 
-    Name = "Node-1"
-  }
-  provisioner "file" {                               # OFF SLOW OFF key error
-    source      = "${path.module}/node.sh"
-    destination = "/tmp/script.sh"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/script.sh",
-      "sudo bash /tmp/script.sh",
-    ]
-  }
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    password    = ""
-    private_key = file("${path.module}/Stepik1.pem")
-    host        = self.public_ip
+    Name = "Node-${count.index + 1}"
   }
 }
 
-// Node2
-resource "aws_instance" "AWS-2" {
-  availability_zone = "eu-north-1a"
-  ami = "ami-0014ce3e52359afbd"
-  instance_type = "t3.micro"
-  key_name = "Stepik1"
-  vpc_security_group_ids = [aws_security_group.AWS-1.id]
-  ebs_block_device {
-    device_name = "/dev/sda1"
-    volume_size = 10
-    volume_type = "standard"
-    tags = {device_name = "Terraform"}
-  }
-  tags = { 
-    Name = "Node-2"
-  }
-  provisioner "file" {                               # OFF SLOW OFF key error
-    source      = "${path.module}/node.sh"
-    destination = "/tmp/script.sh"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/script.sh",
-      "sudo bash /tmp/script.sh",
-    ]
-  }
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    password    = ""
-    private_key = file("${path.module}/Stepik1.pem")
-    host        = self.public_ip
-  }
-}
 
-// Node3
-resource "aws_instance" "AWS-3" {
-  availability_zone = "eu-north-1a"
-  ami = "ami-0014ce3e52359afbd"
-  instance_type = "t3.micro"
-  key_name = "Stepik1"
-  vpc_security_group_ids = [aws_security_group.AWS-1.id]
-  ebs_block_device {
-    device_name = "/dev/sda1"
-    volume_size = 10
-    volume_type = "standard"
-    tags = {device_name = "Terraform"}
-  }
-  tags = { 
-    Name = "Node-3"
-  }
-  provisioner "file" {                               # OFF SLOW OFF key error
-    source      = "${path.module}/node.sh"
-    destination = "/tmp/script.sh"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/script.sh",
-      "sudo bash /tmp/script.sh",
-    ]
-  }
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    password    = ""
-    private_key = file("${path.module}/Stepik1.pem")
-    host        = self.public_ip
-  }
-}
-
-resource "aws_security_group" "AWS-0" {
+resource "aws_security_group" "master_host" {
+  name = "master-host"
   ingress{
     from_port = 22
     to_port = 22
@@ -206,7 +104,8 @@ resource "aws_security_group" "AWS-0" {
   }
 }
 
-resource "aws_security_group" "AWS-1" {
+resource "aws_security_group" "nodes" {
+  name = "nodes"
   ingress{
     from_port = 22
     to_port = 22
@@ -251,105 +150,29 @@ resource "aws_security_group" "AWS-1" {
   }
 }
 
-resource "aws_security_group" "AWS-2" {
-  ingress{
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress{
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress{
-    from_port = 2377
-    to_port = 2377
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress{
-    from_port = 7946
-    to_port = 7946
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress{
-    from_port = 7946
-    to_port = 7946
-    protocol = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress{
-    from_port = 4789
-    to_port = 4789
-    protocol = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress{
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+output "master_host_public_ip" {
+  value = aws_instance.master_host.*.public_ip
 }
 
-resource "aws_security_group" "AWS-3" {
-  ingress{
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress{
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress{
-    from_port = 2377
-    to_port = 2377
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress{
-    from_port = 7946
-    to_port = 7946
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress{
-    from_port = 7946
-    to_port = 7946
-    protocol = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress{
-    from_port = 4789
-    to_port = 4789
-    protocol = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress{
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+output "node_public_ips" {
+  value = aws_instance.node.*.public_ip
 }
 
-output "AWS-0" {
-    value = aws_instance.AWS-0.public_ip
-}
-output "AWS-1" {
-    value = aws_instance.AWS-1.public_ip
-}
-output "AWS-2" {
-    value = aws_instance.AWS-2.public_ip
-}
-output "AWS-3" {
-    value = aws_instance.AWS-3.public_ip
+resource "null_resource" "ansible_inventory" {
+  triggers = {
+    master_host_ips = join(",", aws_instance.master_host.*.public_ip)
+    node_ips        = join(",", aws_instance.node.*.public_ip)
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+cat > ./inventory/hosts.cfg <<EOF
+[master]
+${join("\n", aws_instance.master_host.*.public_ip)}
+
+[nodes]
+${join("\n", aws_instance.node.*.public_ip)}
+EOF
+EOT
+  }
 }
